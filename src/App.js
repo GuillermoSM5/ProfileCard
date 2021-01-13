bc436acfb312e811ce27b74e34c38129d1f57022
GuillermoSM <guillermosotomarquez5@gmail.com>
2021-01-13 17:16:17 -0600

    Se termina proyecto de PorfileCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
-import Bubble from './components/bubbles/bubbles.js'
+import Bubble from './components/bubbles/bubbles.js';
+import Card from './components/card/card.js';
 
 function App() {
   return (
     <>
-    <Bubble />   
+    <Bubble />
+     <Card />    
     </>  
   );
 }
